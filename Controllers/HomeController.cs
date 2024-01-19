0e1d20eadc500c2765da302b6a2217dcb203253a
AlavalaNishanth <103443097+AlavalaNishanth@users.noreply.github.com>
2024-01-19 22:56:12 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using SafteyServicePointMVC.Models;
+
+namespace SafteyServicePointMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Gaurds()
+        {
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(TblUser r)
+        {
+            using (SafetyDBEntities db = new SafetyDBEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    db.TblUsers.Add(r);
+                    db.SaveChanges();
+                  //  ViewBag.message("Register Sucess");
+                    ModelState.Clear();
+                }
+            }
+            return View(r);
+        }
+        [HttpPost]
+        public ActionResult Login(TblUser r1)
+        {
+            using (SafetyDBEntities db = new SafetyDBEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    var log = db.TblUsers.Where(a => a.userName.Equals(r1.userName) && a.password.Equals(r1.password)).FirstOrDefault();
+                    if (log != null)
+                    {
+                        return RedirectToAction("Contact");
+                    }
+                    db.SaveChanges();
+                   // ViewBag.message("Login Sucess");
+                    ModelState.Clear();
+                }
+            }
+            return View(r1);
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SafteyServicePointMVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SafteyServicePointMVC")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ada92a4b-e6ee-42a8-b7e9-5e3bf1e0d44e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
